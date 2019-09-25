d6e2af15e6fa05471e46031b8ab1297a0fea9c15
Keith Heffington <lightingcount_@hotmail.com>
2019-09-25 13:23:05 -0500

    Top Button Script

--- JS/Style.js
+++ JS/Style.js
@@ -1,3 +1,23 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+// Get Button
+var myButton = document.getElementById("topBtn");
+
+// user scrolls down 20px form the top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// user clicks on button
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
